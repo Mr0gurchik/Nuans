3d2e5ab0f89a35ccf7825eb6a5f5888ad2714bc7
MrOgurchik <MrOgurchik@DESKTOP-HECIVUA>
2025-02-03 10:01:23 +0300

    Добавьте файлы проекта.

---
+++ Nuans/Nuans.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int proverkanasimvoli(const string& text, char cel) 
+{
+    int count = 0;
+    for (char i : text) 
+    {
+        if (i == cel) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    string text;
+    char cel;
+
+    cout << "Введите текст: ";
+    getline(cin, text);
+    cout << "Введите цель для поиска: ";
+    cin >> cel;
+
+    int otvet = proverkanasimvoli(text, cel);
+    cout << "Цель встречается " << otvet << " раз" << endl;
+}
